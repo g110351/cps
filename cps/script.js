d2098fad14c8cea7e0a83907b364ec4b81502b07
g110351 <121302269+g110351@users.noreply.github.com>
2023-10-07 19:36:01 +0800

    Update script.js
    updated dynamic cps calculating method

--- cps/script.js
+++ cps/script.js
@@ -14,85 +14,86 @@ let timer;
 let timerCount;
 let clicks = 0;
 let cps = 0;
+let delay = 3;
 let history = [];
+let cpsUpdateInterval;  // Variable for CPS update interval
+
+// Function to update CPS display
+function updateCPS() {
+    if (delay === 0) {
+        cps = clicks / (mode - timerCount + 1);  // Avoid division by zero
+        cpsBtn.textContent = cps.toFixed(2) + " CPS";
+    }
+}
 
 // Function to start timer
 function startTimer() {
-  // Disable mode select and start button
-  modeSelect.disabled = true;
-  startBtn.disabled = true;
+    modeSelect.disabled = true;
+    startBtn.disabled = true;
+    
+    timerCount = mode;
+    timerMinutes.textContent = Math.floor(timerCount / 60).toString().padStart(2, "0");
+    timerSeconds.textContent = (timerCount % 60).toString().padStart(2, "0");
 
-  // Set timer count and display
-  timerCount = mode
-  timerMinutes.textContent = Math.floor(timerCount / 60).toString().padStart(2, "0");
-  timerSeconds.textContent = (timerCount % 60).toString().padStart(2, "0");
+    cpsBtn.disabled = false;
+    cpsUpdateInterval = setInterval(updateCPS, 100);  // Update CPS every 100ms
 
-  // Enable CPS button
-  cpsBtn.disabled = false;
-
-  // Add 1 second delay before starting timer countdown
-  setTimeout(() => {
-    // Start timer countdown
     timer = setInterval(() => {
-      timerCount--;
-      timerMinutes.textContent = Math.floor(timerCount / 60).toString().padStart(2, "0");
-      timerSeconds.textContent = (timerCount % 60).toString().padStart(2, "0");
+        if (delay > 0) {
+            delay--;
+            timerSeconds.textContent = delay.toString().padStart(2, "0");
+        } else {
+            timerCount--;
+            timerMinutes.textContent = Math.floor(timerCount / 60).toString().padStart(2, "0");
+            timerSeconds.textContent = (timerCount % 60).toString().padStart(2, "0");
 
-      // End timer when countdown reaches 0
-      if (timerCount === 0) {
-        clearInterval(timer);
-        endTimer();
-      }
+            if (timerCount === 0) {
+                clearInterval(timer);
+                clearInterval(cpsUpdateInterval);  // Clear CPS update interval when timer ends
+                endTimer();
+            }
+        }
     }, 1000);
-  }, 1000);
 }
 
 // Function to end timer
 function endTimer() {
-  // Disable CPS button
-  cpsBtn.disabled = true;
+    cpsBtn.disabled = true;
+    cps = clicks / mode;
+    cpsBtn.textContent = cps.toFixed(2) + " CPS";
 
-  // Calculate CPS and update display
-  cps = clicks / mode;
-  cpsBtn.textContent = cps.toFixed(2) + " CPS";
+    history.push(cps.toFixed(2));
+    const historyItem = document.createElement("li");
+    historyItem.textContent = cps.toFixed(2) + " CPS";
+    historyList.appendChild(historyItem);
 
-  // Add CPS to history and update display
-  history.push(cps.toFixed(2));
-  const historyItem = document.createElement("li");
-  historyItem.textContent = cps.toFixed(2) + " CPS";
-  historyList.appendChild(historyItem);
+    if (cps > parseFloat(bestRecord.textContent)) {
+        bestRecord.textContent = cps.toFixed(2) + " CPS";
+    }
 
-  // Update best record if necessary
-  if (cps > parseFloat(bestRecord.textContent)) {
-    bestRecord.textContent = cps.toFixed(2) + " CPS";
-  }
-
-  // Enable mode select and start button
-  modeSelect.disabled = false;
-  startBtn.disabled = false;
+    modeSelect.disabled = false;
+    startBtn.disabled = false;
+    delay = 3;
 }
 
-// Event listener for mode select
+// Event listeners
 modeSelect.addEventListener("change", () => {
-  mode = parseInt(modeSelect.value);
+    mode = parseInt(modeSelect.value);
 });
 
-// Event listener for start button
 startBtn.addEventListener("click", () => {
-  clicks = 0;
-  startTimer();
+    clicks = 0;
+    startTimer();
 });
 
-// Event listener for CPS button
 cpsBtn.addEventListener("click", () => {
-  clicks++;
-  cps = clicks / (mode - timerCount);
-  cpsBtn.textContent = cps.toFixed(2) + " CPS";
+    if (delay === 0) {
+        clicks++;
+    }
 });
 
-// Event listener for clear history button
 clearHistoryBtn.addEventListener("click", () => {
-  history = [];
-  historyList.innerHTML = "";
-  bestRecord.textContent = "0 CPS";
+    history = [];
+    historyList.innerHTML = "";
+    bestRecord.textContent = "0 CPS";
 });
